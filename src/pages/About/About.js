db51718576080f6cad1899b006b38f528926b6ef
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-06-14 15:28:10 -0300

    blocked routes of user logout, and create jsx of page About

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,21 @@
-import { styles } from '../About/About.module.css';
+import styles from '../About/About.module.css';
+import { Link } from 'react-router-dom';
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito com React no front-End e FireBase
+        no back-end.
+      </p>
+
+      <Link to="/posts/create" className="btn">
+        Criar Post
+      </Link>
+    </div>
+  );
 };
 
 export default About;
