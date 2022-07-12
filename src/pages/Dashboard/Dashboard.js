1dd4a3b4299e4168ef10227dc1e75170ef64e3ff
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-07-12 20:17:00 -0300

    finished page od delete funcionality

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 import { useAuthValue } from '../../context/AuthContext'
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+import { useDeleteDocument } from '../../hooks/useDeleteDocument'
 
 const Dashboard = () => {
   const { user } = useAuthValue()
   const uid = user.uid
 
   const { documents: posts, loading } = useFetchDocuments('post', null, uid)
-
-  const deleteDocument = (id) => {}
+  const { deleteDocument } = useDeleteDocument('post')
 
   if (loading) {
     return <p>Carregando...</p>
