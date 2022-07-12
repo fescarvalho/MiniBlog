8f4b6886ccf318cf3ac5379d72a947879e120dfb
Fernando Carvalho <fernando_scarvalho@hotmail.com>
2022-07-12 19:50:31 -0300

    page of single post finalized and page of dashboard unde development

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -1,39 +1,45 @@
-import { useState, useEffect } from 'react';
-import { db } from '../firebase/config';
+import { useState, useEffect } from 'react'
+import { db } from '../firebase/config'
 import {
   collection,
   query,
   orderBy,
   onSnapshot,
   where,
-} from 'firebase/firestore';
+} from 'firebase/firestore'
 
 export const useFetchDocuments = (docCollection, search = null, uid = null) => {
-  const [documents, setDocuments] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(null);
+  const [documents, setDocuments] = useState(null)
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(null)
 
   //deal with memory leaks
-  const [cancelled, setCancelled] = useState(false);
+  const [cancelled, setCancelled] = useState(false)
 
   useEffect(() => {
     const loadData = async () => {
-      if (cancelled) return;
+      if (cancelled) return
 
-      setLoading(true);
-      const collectionRef = await collection(db, docCollection);
+      setLoading(true)
+      const collectionRef = await collection(db, docCollection)
 
       try {
-        let q;
+        let q
 
         if (search) {
           q = await query(
             collectionRef,
             where('tagsArray', 'array-contains', search),
             orderBy('createdAt', 'desc')
-          );
+          )
+        } else if (uid) {
+          q = await query(
+            collectionRef,
+            where('uid', '==', uid),
+            orderBy('createdAt', 'desc')
+          )
         } else {
-          q = await query(collectionRef, orderBy('createdAt', 'desc'));
+          q = await query(collectionRef, orderBy('createdAt', 'desc'))
         }
 
         await onSnapshot(q, (querySnapshot) => {
@@ -42,19 +48,19 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
               id: doc.id,
               ...doc.data(),
             }))
-          );
-        });
-        setLoading(false);
+          )
+        })
+        setLoading(false)
       } catch (e) {
-        console.log('errroou');
-        setError(e.message);
-        setLoading(false);
+        console.log('errroou')
+        setError(e.message)
+        setLoading(false)
       }
-    };
-    loadData();
-  }, [docCollection, search, uid, cancelled]);
+    }
+    loadData()
+  }, [docCollection, search, uid, cancelled])
   useEffect(() => {
-    return () => setCancelled(true);
-  }, []);
-  return { documents, loading, error };
-};
+    return () => setCancelled(true)
+  }, [])
+  return { documents, loading, error }
+}
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,35 @@
-import styles from './Dashboard.module.css';
+import styles from './Dashboard.module.css'
+import { Link } from 'react-router-dom'
+
+import { useAuthValue } from '../../context/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 
 const Dashboard = () => {
+  const { user } = useAuthValue()
+  const uid = user.uid
+
+  const { documents: posts, loading } = useFetchDocuments('post', null, uid)
+
   return (
     <div>
-      <h1>Dashboard</h1>
-    </div>
-  );
-};
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts.</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Nao foram encontrados posts...</p>
+          <Link to="/posts/create" className="btn">
+            Criar um post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem Post</p>
+        </div>
+      )}
 
-export default Dashboard;
+      {posts && posts.map((post) => <h3>{post.title}</h3>)}
+    </div>
+  )
+}
+
+export default Dashboard
